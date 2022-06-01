141c42c88a6f250783d30695afba8d9fabf5b1b7
flora <flora@163.com>
2022-06-01 11:34:46 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * @Title:GitTest
+ * @Description:com.atguigu
+ * @author:Bruce
+ * @version:1.0
+ * @create:2022/6/1 11:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
